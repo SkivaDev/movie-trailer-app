c5a8c37fa6c642e9d62b0e0e5066067ee18f4151
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2022-11-16 20:24:14 -0500

    primer commit del proyecto

---
+++ src/componets/MovieCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MovieCard({movie}) {
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
+
+  return (
+    <div className={"movie-card"}>
+      {movie.poster_path 
+      ? <img className={"movie-cover"} src={`${IMAGE_PATH}${movie.poster_path}`}/>
+      : null
+      }
+      <h5>{movie.title}</h5>
+    </div>
+  )
+}
+
+export default MovieCard
