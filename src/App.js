bf276e6936f4e8397e669834a820439e97ffca21
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2022-11-17 16:50:29 -0500

    Movie Search

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import "./App.css";
 import MovieCard from "./componets/MovieCard";
 
 function App() {
-  const API_URL = "https://api.themoviedb.org/3";
-  const [movies, setMovies] = useState([]);
 
-  const fetchMovies = async () => {
+  const API_URL = "https://api.themoviedb.org/3";
+
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover/movie"
     const {
       data: { results },
-    } = await axios.get(`${API_URL}/discover/movie`, {
+    } = await axios.get(`${API_URL}/${type}/movie`, {
       params: {
         api_key: process.env.REACT_APP_MOVIE_API_KEY,
+        query: searchKey
       },
     });
 
@@ -31,10 +36,22 @@ function App() {
     />)
   );
 
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey)
+  }
   return (
     <div className="App">
-      <h1>Hello youtube</h1>
-      <div className={"container"}>{renderMovies()}</div>
+      <header>
+        <h1>Movie Trailer App</h1>
+        <form onSubmit={searchMovies}>
+          <input type="text" onChange={(e) => setSearchKey(e.target.value)}/>
+          <button type={"submit"}>Search!</button>
+        </form>
+      </header>
+      <div className={"container"}>
+        {renderMovies()}
+      </div>
     </div>
   );
 }
