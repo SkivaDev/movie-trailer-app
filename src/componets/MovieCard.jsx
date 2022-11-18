1ea53dd3ee83aaf6a369244abf3e95269ce09bf0
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2022-11-17 19:30:53 -0500

    Changing Movies on Click

--- src/componets/MovieCard.jsx
+++ src/componets/MovieCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function MovieCard({movie}) {
+function MovieCard({movie, selectMovie}) {
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
 
   return (
-    <div className={"movie-card"}>
+    <div className={"movie-card"} onClick={() => selectMovie(movie)}>
       {movie.poster_path 
       ? <img className={"movie-cover"} src={`${IMAGE_PATH}${movie.poster_path}`}/>
       : <div className='movie-placeholder'>No Image Found</div>
